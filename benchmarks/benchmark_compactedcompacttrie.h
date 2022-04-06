fce433724bb374e83f31bfe8bb27121271dc9a72
Dominik Horn <dominik.horn@tum.de>
2022-04-06 13:48:14 -0400

    fixup enable compactTrie

--- benchmarks/benchmark_compactedcompacttrie.h
+++ benchmarks/benchmark_compactedcompacttrie.h
@@ -8,30 +8,30 @@ template <template <typename> typename Searcher>
 void benchmark_32_compactedcompacttrie(
     sosd::Benchmark<uint32_t, Searcher>& benchmark, bool pareto) {
   benchmark.template Run<CompactedCompactTrie<uint32_t, 1>>();
-  // benchmark.template Run<CompactedCompactTrie<uint32_t, 128>>();
-  // if (pareto) {
-  //   benchmark.template Run<CompactedCompactTrie<uint32_t, 256>>();
-  //   benchmark.template Run<CompactedCompactTrie<uint32_t, 512>>();
-  //   benchmark.template Run<CompactedCompactTrie<uint32_t, 1024>>();
-  //   benchmark.template Run<CompactedCompactTrie<uint32_t, 2048>>();
-  //   benchmark.template Run<CompactedCompactTrie<uint32_t, 4096>>();
-  //   benchmark.template Run<CompactedCompactTrie<uint32_t, 8192>>();
-  // }
+  if (pareto) {
+    benchmark.template Run<CompactedCompactTrie<uint32_t, 2>>();
+    benchmark.template Run<CompactedCompactTrie<uint32_t, 4>>();
+    benchmark.template Run<CompactedCompactTrie<uint32_t, 8>>();
+    benchmark.template Run<CompactedCompactTrie<uint32_t, 16>>();
+    benchmark.template Run<CompactedCompactTrie<uint32_t, 32>>();
+    benchmark.template Run<CompactedCompactTrie<uint32_t, 64>>();
+    benchmark.template Run<CompactedCompactTrie<uint32_t, 128>>();
+  }
 }
 
 template <template <typename> typename Searcher>
 void benchmark_64_compactedcompacttrie(
     sosd::Benchmark<uint64_t, Searcher>& benchmark, bool pareto) {
   benchmark.template Run<CompactedCompactTrie<uint64_t, 1>>();
-  // benchmark.template Run<CompactedCompactTrie<uint64_t, 128>>();
-  // if (pareto) {
-  //   benchmark.template Run<CompactedCompactTrie<uint64_t, 256>>();
-  //   benchmark.template Run<CompactedCompactTrie<uint64_t, 512>>();
-  //   benchmark.template Run<CompactedCompactTrie<uint64_t, 1024>>();
-  //   benchmark.template Run<CompactedCompactTrie<uint64_t, 2048>>();
-  //   benchmark.template Run<CompactedCompactTrie<uint64_t, 4096>>();
-  //   benchmark.template Run<CompactedCompactTrie<uint64_t, 8192>>();
-  // }
+  if (pareto) {
+    benchmark.template Run<CompactedCompactTrie<uint64_t, 2>>();
+    benchmark.template Run<CompactedCompactTrie<uint64_t, 4>>();
+    benchmark.template Run<CompactedCompactTrie<uint64_t, 8>>();
+    benchmark.template Run<CompactedCompactTrie<uint64_t, 16>>();
+    benchmark.template Run<CompactedCompactTrie<uint64_t, 32>>();
+    benchmark.template Run<CompactedCompactTrie<uint64_t, 64>>();
+    benchmark.template Run<CompactedCompactTrie<uint64_t, 128>>();
+  }
 }
 
 INSTANTIATE_TEMPLATES(benchmark_32_compactedcompacttrie, uint32_t);
