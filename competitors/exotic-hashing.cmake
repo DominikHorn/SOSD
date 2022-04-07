4b4ae5829c7d581462e30500847d6ac15eb2f32f
Dominik Horn <dominik.horn@tum.de>
2022-04-06 21:47:53 -0400

    upgrade dependency to newest version
    - should enable sfmwhc compression

--- competitors/exotic-hashing.cmake
+++ competitors/exotic-hashing.cmake
@@ -4,6 +4,6 @@ set(EXOTIC_HASHING_LIBRARY exotic-hashing)
 FetchContent_Declare(
   ${EXOTIC_HASHING_LIBRARY}
   GIT_REPOSITORY https://github.com/DominikHorn/exotic-hashing.git
-  GIT_TAG 3bd992b
+  GIT_TAG 49cdd63
   )
 FetchContent_MakeAvailable(${EXOTIC_HASHING_LIBRARY})
